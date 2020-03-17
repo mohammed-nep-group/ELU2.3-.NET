d3fc9c824636b3687e6b6a6e016f4e9b23b22184
youness-arsalane <youness@adaptivity.nl>
2020-03-17 18:59:52 +0100

    update

--- Cinematix/Pages/Index.cshtml.cs
+++ Cinematix/Pages/Index.cshtml.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Cinematix.Data;
+using Cinematix.Models;
 
-namespace Cinematix.Pages
+namespace Cinematix
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Cinematix.Data.CinematixContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Cinematix.Data.CinematixContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<Movie> Movie { get;set; }
 
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movies.ToListAsync();
         }
     }
 }
---
+++ Cinematix/Pages/Movies/Payment.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cinematix.Data;
+using Cinematix.Models;
+
+namespace Cinematix
+{
+    public class PaymentModel : PageModel
+    {
+        private readonly Cinematix.Data.CinematixContext _context;
+
+        public PaymentModel(Cinematix.Data.CinematixContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(Movie.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
